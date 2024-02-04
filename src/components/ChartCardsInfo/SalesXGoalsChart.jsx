580b3e83e65a84ab247c2f5fe8e92bb851e045aa
Gustavo Fernandes <goncalves_017@hotmail.com>
2024-02-04 12:56:47 -0300

    :lipstick:Style: Changed sales chart reponsiveness

--- src/components/ChartCardsInfo/SalesXGoalsChart.jsx
+++ src/components/ChartCardsInfo/SalesXGoalsChart.jsx
@@ -12,54 +12,63 @@ const SalesXGoals = ({ props }) => {
   // defaults.maintainAspectRatio = true;
   // defaults.responsive = true;
   defaults.aspectRatio = 30 / 10;
-  defaults.scales.linear.min = 12000;
+  // defaults.scales.linear.min = 10000;
   return (
-    <Bar
-      data={{
-        labels: props.goalArray.map((goal, index) => index + 1),
-        datasets: [
-          {
-            label: 'Meta',
-            data: props.goalArray.map((goal, index) => goal),
-            backgroundColor: '#000',
-            borderColor: '#000',
-            type: 'line',
-          },
-          {
-            label: 'Venda',
-            data: props.saleArray.map((sale, index) => sale),
-            backgroundColor: '#007041',
-            borderColor: '#000',
-            type: 'bar',
-          },
-        ],
-      }}
-      options={{
-        y: {
-          ticks: {
-            format: { maximumFractionDigits: 2, minimumFractionDigits: 2 },
-          },
-        },
-        interaction: {
-          intersect: false,
-          mode: 'index',
-        },
-        plugins: {
-          tooltip: {
-            callbacks: {
-              footer: function (tooltipItems) {
-                return `P&L: ${(
-                  tooltipItems[1].raw - tooltipItems[0].raw
-                ).toLocaleString('pt-BR', {
-                  style: 'decimal',
-                  minimumFractionDigits: 2,
-                })}`;
+    <div className="overflow-auto">
+      <div className="min-h-[300px] min-w-[600px]">
+        <Bar
+          data={{
+            labels: props.goalArray.map((goal, index) => index + 1),
+            datasets: [
+              {
+                label: 'Meta',
+                data: props.goalArray.map((goal, index) => goal),
+                backgroundColor: '#000',
+                borderColor: '#000',
+                type: 'line',
+              },
+              {
+                label: 'Venda',
+                data: props.saleArray.map((sale, index) => sale),
+                backgroundColor: '#007041',
+                borderColor: '#000',
+                type: 'bar',
+              },
+            ],
+          }}
+          options={{
+            maintainAspectRatio: false,
+            y: {
+              grace: '5%',
+              min: 12000,
+              // max: 24000,
+              ticks: {
+                format: { maximumFractionDigits: 2, minimumFractionDigits: 2 },
+                stepSize: 3000,
               },
             },
-          },
-        },
-      }}
-    />
+            interaction: {
+              intersect: false,
+              mode: 'index',
+            },
+            plugins: {
+              tooltip: {
+                callbacks: {
+                  footer: function (tooltipItems) {
+                    return `P&L: ${(
+                      tooltipItems[1].raw - tooltipItems[0].raw
+                    ).toLocaleString('pt-BR', {
+                      style: 'decimal',
+                      minimumFractionDigits: 2,
+                    })}`;
+                  },
+                },
+              },
+            },
+          }}
+        />
+      </div>
+    </div>
   );
 };
 
